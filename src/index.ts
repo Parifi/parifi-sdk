1e1400194c1bd85600c696da5391c42cc3b5b096
racso <oscaremiliolugo@gmail.com>
2025-05-05 11:05:20 -0400

    add distribution to sdk

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { PythConfig, RelayerConfig, RpcConfig, SubgraphConfig } from './interfac
 import { Gelato } from './relayers/gelato';
 import { Perps } from './perps';
 import { Pimlico } from './relayers';
+import { RewardDistribution } from './distribution';
 
 export * from './common';
 export * from './relayers/gelato/gelato-function';
@@ -20,6 +21,7 @@ export class ParifiSdk {
     gelato: Gelato;
     pimlico: Pimlico;
   };
+  rewardDistribution: RewardDistribution;
 
   constructor(
     rpcConfig: RpcConfig,
@@ -34,6 +36,7 @@ export class ParifiSdk {
       gelato: new Gelato(relayerConfig['gelatoConfig'], rpcConfig),
       pimlico: new Pimlico(relayerConfig['pimlicoConfig'], rpcConfig, subgraphConfig),
     };
+    this.rewardDistribution = new RewardDistribution();
   }
 
   async init() {
